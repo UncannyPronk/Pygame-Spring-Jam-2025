c479f568c8c7e1f4d4670a818918333a55aaaca5
Baturay <baturayturan@gmail.com>
2025-04-27 17:24:04 +0900

    Move control constants to config.py for better organization and maintainability

--- objs.py
+++ objs.py
@@ -1,12 +1,6 @@
 import pygame, random, math
+from config import *
 
-LEFT = pygame.K_q
-RIGHT = pygame.K_d
-UP = pygame.K_z
-DOWN = pygame.K_s
-ATTACK = pygame.K_e
-ATTACK2 = pygame.K_a
-SPATIAL = pygame.K_SPACE
 
 
 
